c1dce7f27a16fc5c370c26b2fb0d698ffd22a45e
Arturo Grau <argrau3@gmail.com>
2025-07-27 14:55:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# git_alias_manager
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/git_alias_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.git_alias_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
